feba1d5b4578bfe7d567983097d2e41dc4e3e821
Albedo <albedo.geo@hotmail.com>
2024-12-09 18:21:33 +0800

    fix: 发布脚本不应删除pack目录

--- Publish.ps1
+++ Publish.ps1
@@ -5,14 +5,10 @@ if (Test-Path $publishDir) {
 	Remove-Item $publishDir -Recurse -Force
 }
 
-if (Test-Path $packDir) {
-	Remove-Item $packDir -Recurse -Force
-}
-
 dotnet publish .\VelopackDemo\VelopackDemo.csproj -c Release -o $publishDir
 
 # 输入版本号
 $version = Read-Host "请输入版本号（形如 1.0.0）"
 
 vpk pack -u VelopackDemo -v $version -p $publishDir -e VelopackDemo.exe -o $packDir -i VelopackDemo\icon.ico `
---packAuthors Albedo --delta None
+	--packAuthors Albedo --delta None
