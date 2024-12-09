da9416634e59c7819d70a504a69186f47cc857a4
Albedo <albedo.geo@hotmail.com>
2024-12-09 17:19:19 +0800

    feat: 添加检查更新的逻辑

--- VelopackDemo/MainWindow.xaml.cs
+++ VelopackDemo/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using Velopack;
 
 namespace VelopackDemo;
 
@@ -7,5 +8,37 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private async void Window_Loaded(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            await UpdateMyAppAsync();
+        }
+        catch (Exception ex)
+        {
+            // 检查更新失败，或其他问题
+            MessageBox.Show($"检查更新失败。原因：\n{ex}", "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+
+    private static async Task UpdateMyAppAsync()
+    {
+        var mgr = new UpdateManager("https://github.com/albedo-geo/VelopackDemo/releases");
+
+        if (!mgr.IsInstalled)
+            return;
+
+        var newVersion = await mgr.CheckForUpdatesAsync();
+        if (newVersion is null)
+            return;
+
+        var result = MessageBox.Show($"发现新版本：{newVersion}\n当前版本：{mgr.CurrentVersion}\n是否更新？", "版本更新", MessageBoxButton.YesNo);
+        if (result == MessageBoxResult.Yes)
+        {
+            await mgr.DownloadUpdatesAsync(newVersion);
+            mgr.ApplyUpdatesAndRestart(newVersion);
+        }
     }
 }
