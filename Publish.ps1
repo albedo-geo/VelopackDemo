56a63cf95ea080b67acdba635db500a359efe353
Albedo <albedo.geo@hotmail.com>
2024-12-09 17:47:23 +0800

    chore: 添加发布脚本

---
+++ Publish.ps1
@@ -0,0 +1,18 @@
+﻿$publishDir = "publish"
+$packDir = "pack"
+
+if (Test-Path $publishDir) {
+	Remove-Item $publishDir -Recurse -Force
+}
+
+if (Test-Path $packDir) {
+	Remove-Item $packDir -Recurse -Force
+}
+
+dotnet publish .\VelopackDemo\VelopackDemo.csproj -c Release -o $publishDir
+
+# 输入版本号
+$version = Read-Host "请输入版本号（形如 1.0.0）"
+
+vpk pack -u VelopackDemo -v $version -p $publishDir -e VelopackDemo.exe -o $packDir -i VelopackDemo\icon.ico `
+--packAuthors Albedo --delta None
