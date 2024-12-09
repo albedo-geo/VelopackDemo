f8f14acc32efec4076eeff551c605ed7ef4ce8ec
Albedo <albedo.geo@hotmail.com>
2024-12-09 16:58:31 +0800

    添加项目文件

---
+++ VelopackDemo/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace VelopackDemo;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
---
+++ VelopackDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using Velopack;
+
+namespace VelopackDemo;
+
+class Program
+{
+    [STAThread]
+    static void Main()
+    {
+        VelopackApp.Build().Run();
+
+        var app = new App();
+        app.InitializeComponent();
+        app.Run();
+    }
+}
